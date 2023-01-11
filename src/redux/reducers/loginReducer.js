a9520443c3b05322014cf92848e68ae00e3c8777
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-01-11 15:41:51 -0300

    create initial redux

---
+++ src/redux/reducers/loginReducer.js
@@ -0,0 +1,15 @@
+const INITIAL_STATE = {
+  name: '',
+  assertions: '',
+  score: 0,
+  gravatarEmail: '',
+};
+
+const loginReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default loginReducer;
