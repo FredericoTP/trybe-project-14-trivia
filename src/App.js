6e29f7df982732c310d4742aeb7d133a3525a188
Frederico Tavares Pessoa <fredericotp@hotmail.com>
2023-01-12 17:04:27 -0300

    Req 5

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Login from './pages/Login';
 import Rankings from './pages/Rankings';
 import Settings from './pages/Settings';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/settings" component={ Settings } />
       <Route exact path="/rankings" component={ Rankings } />
+      <Route exact path="/game" component={ Game } />
       <Route exact path="/" component={ Login } />
     </Switch>
   );
